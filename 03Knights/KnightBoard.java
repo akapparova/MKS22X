3a9ed0b8dbb878afdadd35f3314631ee4961594c
akapparova <akapparova@stuy.edu>
2017-02-27 16:39:47 -0500

    done, tweaked toString

--- 03Knights/KnightBoard.java
+++ 03Knights/KnightBoard.java
@@ -3,28 +3,45 @@ public class KnightBoard{
 
     public KnightBoard(int startingRows, int startingCols) {
 	board = new int[startingRows][startingCols];
-    }
-
-    public String toString(){ //blank if you never called solve or when there is no solution
-
+	for (int i = 0; i < (startingRows * startingCols); i ++){
+	    int row = i / startingRows;
+	    int col = i % startingCols;
+	    board[row][col] = 0;
+	}
     }
 
     public void solve() {
-	solveH();
+	solveH(0, 0, 1);
     }
 
-    private boolean solveH(int row ,int col, int level) {// level is the # of the knight
-	if (level > row * col){
-	    return true;
+    private boolean solveH(int row, int col, int level) {
+	if (level > (board.length * board[0].length)) return true;
+	if (row >= board.length || col >= board[0].length || row < 0 || col < 0) return false;
+	if (board[row][col] == 0) {
+	    board[row][col] = level;
+	    if (solveH(row - 2, col + 1, level + 1) ||
+		solveH(row - 2, col - 1, level + 1) ||
+	        solveH(row - 1, col + 2, level + 1) ||
+		solveH(row + 2, col + 1, level + 1) ||
+		solveH(row + 1, col + 2, level + 1) ||
+		solveH(row + 2, col - 1, level + 1) ||
+		solveH(row + 1, col - 2, level + 1) ||
+		solveH(row - 1, col - 2, level + 1))
+		{return true;}
+	    board[row][col] = 0;
 	}
-	else{
-	    if (isOnGoodSpot()){
-		
+	return false;
+    }
+
+    public String toString(){
+	String ans = "";
+	for(int i = 0; i < board.length; i ++){
+	    for(int j = 0; j < board.length; j ++){
+		ans  = ans  + board[j][i] + " ";
 	    }
+	    ans += "\n";
 	}
-    }
-
-    private boolean isOnGoodSpot(){
+	return ans;
     }
 
 }
