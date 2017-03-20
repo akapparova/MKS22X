57bbf5eb65bdbb1c66d339c371e2c7d6349bd34a
akapparova <akapparova@users.noreply.github.com>
2017-03-20 00:05:17 -0400

    Add files via upload
    Quick Select and Sort are both tested and completed.

--- 07Quick/Quick.java
+++ 07Quick/Quick.java
@@ -1,3 +1,5 @@
+import java.util.*;
+
 public class Quick{
 
     public static void swap(int[] ary, int i, int j){
@@ -23,12 +25,24 @@ public class Quick{
 	return pivoting;
     }
 
-    public static String print(int[] ary){
+    public static String printArray(int[] ary){
 	String ans = "";
 	for (int i = 0; i < ary.length; i ++){
 	    ans += (ary[i] + ", ");
 	}
 	return ans;
+    }
+
+    public static void quickSort(int[] ary){
+	quickSortH(ary, 0, (ary.length - 1));
+    }
+    
+    private static void quickSortH(int[] ary, int start, int end){
+	if (((end - start) + 1) > 1){
+	    int div = partition (ary, start, end);
+	    quickSortH(ary, start, div);
+	    quickSortH(ary, div + 1, end);
+	}
     }
 		   
     public static int quickselect(int[] ary, int k){
@@ -36,17 +50,10 @@ public class Quick{
     }
 
     public static int selectH(int[] ary, int k, int start, int end){
-	int boundarystart;
-	int boundaryend;
-	if (partition(ary, start, end) < k){
-	    ;
-    }
-
-    public static void main(String[] args){
-	int[] data = {10,9,8,7,6,5,5,5,5,5,5,4,3,2,1,0};
-	System.out.println(partition(data, 0, 5));
-	System.out.println(print(data));
-	System.out.println(partition(data, 5, 8));
-	System.out.println(print(data));
+        int pivot = partition (ary, start ,end);
+	if (pivot == k) return ary[pivot];
+	if (pivot > k) return selectH(ary, k, start, pivot);
+	if (pivot < k) return selectH(ary, k, pivot + 1, end);
+	return ary[k];
     }
 }
