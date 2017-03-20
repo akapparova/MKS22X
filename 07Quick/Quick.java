cd22313c97a3208ab1600beddec01c8fc09ccff7
akapparova <akapparova@stuy.edu>
2017-03-20 00:04:04 -0400

    Finished QuickSelect and QuickSort. Both methods and helper functions are tested.

--- 07Quick/Quick.java
+++ 07Quick/Quick.java
@@ -1,3 +1,5 @@
+import java.util.*;
+
 public class Quick{
 
     public static void swap(int[] ary, int i, int j){
@@ -23,26 +25,35 @@ public class Quick{
 	return pivoting;
     }
 
-    public static String print(int[] ary){
+    public static String printArray(int[] ary){
 	String ans = "";
 	for (int i = 0; i < ary.length; i ++){
 	    ans += (ary[i] + ", ");
 	}
 	return ans;
     }
-		   
 
+    public static void quickSort(int[] ary){
+	quickSortH(ary, 0, (ary.length - 1));
+    }
+    
+    private static void quickSortH(int[] ary, int start, int end){
+	if (((end - start) + 1) > 1){
+	    int div = partition (ary, start, end);
+	    quickSortH(ary, start, div);
+	    quickSortH(ary, div + 1, end);
+	}
+    }
+		   
     public static int quickselect(int[] ary, int k){
-	return selectH(ary, k, 0, ary.length -1);
+	return selectH(ary, k, 0, ary.length - 1);
     }
 
     public static int selectH(int[] ary, int k, int start, int end){
-	return 1;
-    }
-
-    public static void main(String[] args){
-	int[] ary = {4, 3, 5, 77, 100, 2, 41, 32, 45, 21, 11};
-	System.out.println(partition(ary, 0, 5));
-	System.out.println(print(ary));
+        int pivot = partition (ary, start ,end);
+	if (pivot == k) return ary[pivot];
+	if (pivot > k) return selectH(ary, k, start, pivot);
+	if (pivot < k) return selectH(ary, k, pivot + 1, end);
+	return ary[k];
     }
 }
