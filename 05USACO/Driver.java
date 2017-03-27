470767636904774b9d74666ed5fc1ba3f91532b4
akapparova <akapparova@stuy.edu>
2017-03-27 18:22:00 -0400

    Linked Lists

--- 05USACO/Driver.java
+++
@@ -1,7 +0,0 @@
-public class Driver{
-    public static void main(String[]args){
-        USACO x = new USACO(); 
-	System.out.println(x.bronze("makelake.in"));
-	System.out.println("342144");
-    }
-}
