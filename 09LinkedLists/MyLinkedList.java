fd90b6ee3db339e8777bfbc3da2f6e5a005b1529
akapparova <akapparova@stuy.edu>
2017-03-29 21:29:19 -0400

    toString working, add not working, LNode done

--- 09LinkedLists/MyLinkedList.java
+++ 09LinkedLists/MyLinkedList.java
@@ -1,26 +1,53 @@
 public class MyLinkedList{
-    Lnode start, end;
+    LNode start, end;
     int size;
 
     public MyLinkedList(){}
 
-    public boolean add(int value);//adds the value to end (try adding to the front first, for testing purposes)
+    public boolean add(int value) {
+	LNode n = new LNode(value);
+	LNode ne = start;
+	for (int i = 0; i < size - 1; i ++) {
+	    ne = ne.next;
+	}
+	ne.next = n;
+	size += 1;
+	return true;
+    }
+    
     public int size(){
 	return size;
     }
-    public String toString();//returns a string representation of the list of n elements formatted like: [ v0, v1, v2, v3, ... vn-1] or []
-    public int get(int index);// return the value of the element at the specified index (0 based)
-    public int set(int index, int newvalue);//change the value of the element at the specified index to the newValue, return the old value  
+    
+    public String toString(){
+	String answer = "[";
+	LNode lnode = start;
+	while(lnode != null){
+	    if (lnode.next == null)
+		answer += " " + lnode.value + "]";
+	    else{
+		answer += " " + lnode.value + ",";
+	    }
+
+	    lnode = lnode.next;
+	}
+	return answer;
+    }
+    
+    /*public int get(int index);// return the value of the element at the specified index (0 based)
+      public int set(int index, int newvalue);//change the value of the element at the specified index to the newValue, return the old value  
 
 
-    public int indexOf(int value);//returns the index of the 1st occurrence of the value in the linked list, -1 if not found.
-    public void add(int index, int newValue);// insert a new element at the specified index, 0 at the front, size() at the end. 
-    public int remove(int index);// remove the element at the specified index, returns the value removed		       
-
+      public int indexOf(int value);//returns the index of the 1st occurrence of the value in the linked list, -1 if not found.
+      public void add(int index, int newValue);// insert a new element at the specified index, 0 at the front, size() at the end. 
+      public int remove(int index);// remove the element at the specified index, returns the value removed		       
+    */
 
     private class LNode{
 	private int value;
 	private LNode next;
+
+	public LNode(){}
 
 	public LNode(int val){
 	    this.value = val;
@@ -30,5 +57,9 @@ public class MyLinkedList{
 	    this.value = val;
 	    this.next = nextone;
 	}
+    }
+
+    public static void main(String[] args){
+
     }
 }
