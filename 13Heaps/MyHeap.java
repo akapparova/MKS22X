9f24000cde81d8b2554022bc9429363cf2515d8d
akapparova <akapparova@stuy.edu>
2017-05-03 22:02:53 -0400

    Updating directory again

---
+++ 13Heaps/MyHeap.java
@@ -0,0 +1,11 @@
+public class MyHeap{
+    public MyHeap(){}//construct empty max heap
+    public MyHeap(boolean val){} //true: construct empty max heap, false: construct empty min heap.
+
+    public void add(String s){}
+    public String remove(){}
+    public String peek(){}
+
+    private void pushUp(){}
+    private void pushDown(){}
+}
